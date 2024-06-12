4f0beae702d1d6fb0a49f397a8dc89103a0cac3e
Eric Lin <ericlin0@meta.com>
2024-06-12 13:27:12 -0700

    Pretttier

--- src/App.jsx
+++ src/App.jsx
@@ -1,159 +1,210 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import BrowseMode from './components/BrowseMode/BrowseMode';
-import SearchMode from './components/SearchMode/SearchMode';
-import MovieList from './components/Movie/MovieList/MovieList';
-import api from './api';
+import { useEffect, useState } from "react";
+import "./App.css";
+import BrowseMode from "./components/BrowseMode/BrowseMode";
+import SearchMode from "./components/SearchMode/SearchMode";
+import MovieList from "./components/Movie/MovieList/MovieList";
+import api from "./api";
+
+const DEFAULT_API_REQ_DATA = {
+    search: "",
+    page: 1,
+    filters: 0, // if not 0, there is at least 1 filter, handle that
+    genres: [],
+    dateRange: [Date(-8640000000000000), Date(8640000000000000)],
+    ratingRange: [0, 10],
+};
+
+const SORT_MODES = {
+    None: 0,
+    RatingDesc: 1,
+    RatingAsc: 2,
+    "A-Z": 3,
+    "Z-A": 4,
+    Latest: 5,
+    Earliest: 6,
+};
 
 function App() {
-  const [movieData, setMovieData] = useState([]);
-  const [apiReqData, setApiReqData] = useState({ 'search': '', 'page': 1 });
-  const [searchQuery, setSearchQuery] = useState('');
-  const [searchMode, setSearchMode] = useState(false); // [false: now playing, true: search
-  const [sortMode, setSortMode] = useState(0);
+    const [movieData, setMovieData] = useState([]);
+    const [apiReqData, setApiReqData] = useState(DEFAULT_API_REQ_DATA);
+    const [searchMode, setSearchMode] = useState(false); // [false: now playing, true: search
+    const [sortMode, setSortMode] = useState(0);
 
-  const SORT_MODES = {
-    'None': 0,
-    'RatingDesc': 1,
-    'RatingAsc': 2,
-    'A-Z': 3,
-    'Z-A': 4,
-    'Latest': 5,
-    'Earliest': 6
-  };
+    const { fetchPageData, fetchSearchData } = api();
+    // =================================================================================
+    // handle when user requests new data
+    useEffect(() => {
+        if (apiReqData["search"] == "") {
+            if (apiReqData["filters"] == 0) {
+                setSearchMode(false);
+                fetchPageData(
+                    apiReqData["page"],
+                    apiReqData["page"] != 1,
+                    setMovieData
+                );
+            } else {
+                console.log("HI");
+            }
+        } else {
+            setSearchMode(true);
+            fetchSearchData(
+                apiReqData["search"],
+                apiReqData["page"],
+                apiReqData["page"] != 1,
+                setMovieData
+            );
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [apiReqData]);
 
-  const { fetchPageData, fetchSearchData } = api();
-  // =================================================================================
-  // handle when user requests new data
-  useEffect(() => {
-    if (apiReqData['search'] == '') {
-      setSearchMode(false);
-      fetchPageData(apiReqData['page'], (apiReqData['page'] != 1), setMovieData);
-    }
-    else {
-      setSearchMode(true);
-      fetchSearchData(searchQuery, apiReqData['page'], (apiReqData['page'] != 1), setMovieData);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [apiReqData]);
+    // sort movie data based on sort mode
+    useEffect(() => {
+        switch (sortMode) {
+            case SORT_MODES["None"]:
+                break;
 
-  // sort movie data based on sort mode
-  useEffect(() => {
-    switch (sortMode) {
-      case SORT_MODES['None']:
-        break;
+            case SORT_MODES["RatingDesc"]:
+                setMovieData((oldData) =>
+                    [...oldData].sort(
+                        (a, b) => b["vote_average"] - a["vote_average"]
+                    )
+                );
+                break;
 
-      case SORT_MODES['RatingDesc']:
-        setMovieData((oldData) =>
-          [...oldData].sort((a, b) => b['vote_average'] - a['vote_average']));
-        break;
+            case SORT_MODES["RatingAsc"]:
+                setMovieData((oldData) =>
+                    [...oldData].sort(
+                        (a, b) => a["vote_average"] - b["vote_average"]
+                    )
+                );
+                break;
 
-      case SORT_MODES['RatingAsc']:
-        setMovieData((oldData) =>
-          [...oldData].sort((a, b) => a['vote_average'] - b['vote_average']));
-        break;
+            case SORT_MODES["A-Z"]:
+                setMovieData((oldData) =>
+                    [...oldData].sort((a, b) =>
+                        a["title"].localeCompare(b["title"])
+                    )
+                );
+                break;
 
-      case SORT_MODES['A-Z']:
-        setMovieData((oldData) =>
-          [...oldData].sort((a, b) => a['title'].localeCompare(b['title'])));
-        break;
+            case SORT_MODES["Z-A"]:
+                setMovieData((oldData) =>
+                    [...oldData].sort((a, b) =>
+                        b["title"].localeCompare(a["title"])
+                    )
+                );
+                break;
 
-      case SORT_MODES['Z-A']:
-        setMovieData((oldData) =>
-          [...oldData].sort((a, b) => b['title'].localeCompare(a['title'])));
-        break;
+            case SORT_MODES["Latest"]:
+                setMovieData((oldData) =>
+                    [...oldData].sort(
+                        (a, b) =>
+                            new Date(b["release_date"]) -
+                            new Date(a["release_date"])
+                    )
+                );
+                break;
 
-      case SORT_MODES['Latest']:
-        setMovieData((oldData) =>
-          [...oldData].sort((a, b) => new Date(b['release_date']) - new Date(a['release_date'])));
-        break;
+            case SORT_MODES["Earliest"]:
+                setMovieData((oldData) =>
+                    [...oldData].sort(
+                        (a, b) =>
+                            new Date(a["release_date"]) -
+                            new Date(b["release_date"])
+                    )
+                );
+                break;
 
-      case SORT_MODES['Earliest']:
-        setMovieData((oldData) =>
-          [...oldData].sort((a, b) => new Date(a['release_date']) - new Date(b['release_date'])));
-        break;
+            default:
+                break;
+        }
+    }, [sortMode]);
 
-      default:
-        break;
-    }
-  }, [sortMode]);
+    // =================================================================================
 
-  // =================================================================================
+    // helper functions
+    const loadMore = () => {
+        setApiReqData({
+            search: apiReqData["search"],
+            page: apiReqData["page"] + 1,
+            filters: apiReqData["filters"],
+            genres: [],
+            dateRange: apiReqData["dateRange"],
+            ratingRange: apiReqData["ratingRange"],
+        });
+    };
 
-  // helper functions
-  const loadMore = () => {
-    let searchMode = apiReqData['search'];
-    let page = apiReqData['page'];
-    setApiReqData({ 'search': searchMode, 'page': page + 1 });
-  };
+    const updateSearchMode = (mode) => {
+        setSearchMode(mode);
+    };
 
-  const updateSearchMode = (mode) => {
-    setSearchMode(mode);
-  };
+    const handleSortChange = (event) => {
+        setSortMode(SORT_MODES[event.target.value]);
+    };
 
-  const handleSortChange = (event) => {
-    setSortMode(SORT_MODES[event.target.value]);
-  };
+    // =================================================================================
+    return (
+        <>
+            <header>
+                <div>
+                    <h1>Flixster</h1>
+                </div>
+            </header>
+            <main>
+                <div id="filters">
+                    <div className="flex center-v">
+                        <div
+                            className={
+                                "button " + (searchMode ? "" : "button-active")
+                            }
+                            onClick={() => {
+                                updateSearchMode(false);
+                                setApiReqData(DEFAULT_API_REQ_DATA);
+                            }}
+                        >
+                            Now Playing
+                        </div>
+                        <div
+                            className={
+                                "button " + (searchMode ? "button-active" : "")
+                            }
+                            onClick={() => updateSearchMode(true)}
+                        >
+                            Search
+                        </div>
+                    </div>
 
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
+                    <div className="center-v">
+                        <label htmlFor="sort-dropdown">Sort by: &nbsp;</label>
+                        <select id="sort-dropdown" onChange={handleSortChange}>
+                            <option value="None">None</option>
+                            <option value="RatingDesc">Rating Descending</option>
+                            <option value="RatingAsc">Rating Ascending</option>
+                            <option value="A-Z">A-Z</option>
+                            <option value="Z-A">Z-A</option>
+                            <option value="Latest">Latest Release</option>
+                            <option value="Earliest">Earliest Release</option>
+                        </select>
+                    </div>
 
-  const search = () => {
-    setApiReqData({ 'search': searchQuery, 'page': 1 });
-  };
+                    <BrowseMode searchMode={searchMode} />
 
-  // =================================================================================
-  return (
-    <>
-      <header>
-        <div>
-          <h1>Flixster</h1>
-        </div>
-      </header>
-      <main>
-        <div id='filters'>
-          <div className='flex center-v'>
-            <div className={'button ' + (searchMode ? '' : 'button-active')} 
-              onClick={() => {updateSearchMode(false); setApiReqData({ 'search': '', 'page': 1 })}}>Now Playing</div>
-            <div className={'button ' + (searchMode ? 'button-active' : '')} onClick={() => updateSearchMode(true)}>Search</div>
-          </div>
+                    <SearchMode
+                        searchMode={searchMode}
+                        apiSetFunct={setApiReqData}
+                    />
+                </div>
 
-          <div className='center-v'>
-            <label htmlFor='sort-dropdown'>Sort by: &nbsp;</label>
-            <select id='sort-dropdown' onChange={handleSortChange}>
-              <option value='None'>None</option>
-              <option value='RatingDesc'>Rating Descending</option>
-              <option value='RatingAsc'>Rating Ascending</option>
-              <option value='A-Z'>A-Z</option>
-              <option value='Z-A'>Z-A</option>
-              <option value='Latest'>Latest Release</option>
-              <option value='Earliest'>Earliest Release</option>
-            </select>
-          </div>
+                <MovieList movies={movieData} />
 
-          <BrowseMode
-            searchMode={searchMode}
-          />
+                <button id="load-button" onClick={loadMore}>
+                    Load More
+                </button>
+            </main>
 
-          <SearchMode 
-            searchMode={searchMode} 
-            handleSearchChange={handleSearchChange} 
-            search={search} 
-          />
-
-        </div>
-
-        <MovieList movies={movieData} />
-
-        <button id='load-button' onClick={loadMore}>
-          Load More
-        </button>
-      </main>
-
-      <footer>Flixster</footer>
-    </>
-  );
+            <footer>Flixster</footer>
+        </>
+    );
 }
 
 export default App;
--- src/components/SearchMode/SearchMode.jsx
+++ src/components/SearchMode/SearchMode.jsx
@@ -1,21 +1,43 @@
 import './SearchMode.css';
 import propTypes from 'prop-types';
+import { useState } from 'react';
 
 SearchMode.propTypes = {
     searchMode: propTypes.bool.isRequired,
-    handleSearchChange: propTypes.func.isRequired,
-    search: propTypes.func.isRequired,
-}
+    apiSetFunct: propTypes.func.isRequired,
+};
 
 function SearchMode(props) {
-    return (
-        <div className='center-v' style={{display: (props.searchMode ? 'block' : 'none')}}>
-        <input
-            type='text'
-            placeholder='Search'
-            onChange={props.handleSearchChange}
-        />
-        <button className='button' onClick={props.search}>Search</button>
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const search = () => {
+        props.apiSetFunct({
+        search: searchQuery,
+        page: 1,
+        filters: 0,
+        genres: [],
+        dateRange: [Date(-8640000000000000), Date(8640000000000000)],
+        ratingRange: [0, 10],
+        });
+    };
+
+  return (
+        <div
+        className="center-v"
+        style={{ display: props.searchMode ? "block" : "none" }}
+        >
+            <input
+                type="text"
+                placeholder="Search"
+                onChange={handleSearchChange}
+            />
+            <button className="button" onClick={search}>
+                Search
+            </button>
         </div>
     );
 }
